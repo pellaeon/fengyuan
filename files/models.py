997ce70afed2d73f78269d86218003135e1317e0
Pellaeon Lin <nfsmwlin@gmail.com>
2016-10-16 09:53:18 +0800

    Add# Inode, Directory models

--- files/models.py
+++ files/models.py
@@ -1,8 +1,11 @@
+import os
 import uuid
+
 from django.db import models
-from polymorphic_tree.models import (PolymorphicMPTTModel, 
-    PolymorphicTreeForeignKey
-)
+from django.utils import timezone
+from polymorphic_tree.models import (PolymorphicMPTTModel,
+                                     PolymorphicTreeForeignKey)
+
 
 class FYNode(PolymorphicMPTTModel):
     """
@@ -11,12 +14,30 @@ class FYNode(PolymorphicMPTTModel):
     FYNode should be used as base class for all data hierarchy object models.
     """
     parent = PolymorphicTreeForeignKey('self', blank=True, null=True,
-            related_name='children')
+                                       related_name='children')
     name = models.CharField(max_length=200)
-    gid = models.UUIDField(default=uuid.uuid4, editable=False) # global id
+    gid = models.UUIDField(default=uuid.uuid4, editable=False)  # global id
 
-# TODO class Inode(FYNode), actual file/dir existing in filesystem
-# TODO class Directory(Inode)
+
+class Inode(FYNode):
+    """
+    Actual file/dir existing in filesystem
+    """
+    owner = models.ForeignKey(User)
+    created = models.DateTimeField(default=timezone.now)
+    updated = models.DateTimeField(default=timezone.now)
+    size = models.BigIntegerField(
+        null=True, blank=True)  # null means unknown size
+
+
+class Directory(Inode):
+    path = models.FilePathField(
+        path=owner.profile.home_dir, recursive=True, allow_folders=True, allow_files=False)
+
+    @property
+    def name(self):
+        return os.path.basename(self.path)
+
 # TODO class RootDirectory(Directory)
 # Future: different FileStorage class configured in RootDirectory
 # TODO class File(Inode)
--- user/models.py
+++ user/models.py
@@ -1,6 +1,14 @@
+import os
+
+from django.contrib.auth.models import User
 from django.db import models
-from django.contrib.auth.models import User, Group
+from django.settings import MEDIA_ROOT
+
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, related_name='profile')
     display_name = models.CharField(max_length=32, blank=True, null=True)
+
+    @property
+    def home_dir(self):
+        return os.path.join(MEDIA_ROOT, user.username)
