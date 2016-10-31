db577eaf2c7709707f33b4e1ee16eef6a10f697a
shunyi <M157q.tw@gmail.com>
2016-10-31 16:39:49 +0800

    Rename USER_ROOT_DIR_NAME to USER_FILES_DIR_NAME.
    Get the path of user_dir from user.profile.home_dir_path.

--- files/management/commands/scan.py
+++ files/management/commands/scan.py
@@ -1,6 +1,5 @@
 import os
 
-from django.conf import settings
 from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.management.base import BaseCommand, CommandError
@@ -11,7 +10,7 @@ from files.models import Directory, File
 # https://docs.djangoproject.com/en/1.10/howto/custom-management-commands/
 class Command(BaseCommand):
     help = 'Recursively scans the directories and files of the "files/" dir for a specific username, and save those direcoties and files into database. '
-    USER_ROOT_DIR_NAME = 'files'
+    USER_FILES_DIR_NAME = 'files'
 
     def add_arguments(self, parser):
         parser.add_argument('username', type=str,
@@ -26,15 +25,15 @@ class Command(BaseCommand):
             raise CommandError(
                 "username: {} doesn't exists in the User model.".format(username))
 
-        user_dir = os.path.join(settings.MEDIA_ROOT, user.username)
+        user_dir = user.profile.home_dir_path
         if not os.path.exists(user_dir):
             os.makedirs(user_dir)
 
-        user_root_dir = os.path.join(user_dir, self.USER_ROOT_DIR_NAME)
-        if not os.path.exists(user_root_dir):
-            os.makedirs(user_root_dir)
+        user_files_dir = os.path.join(user_dir, self.USER_FILES_DIR_NAME)
+        if not os.path.exists(user_files_dir):
+            os.makedirs(user_files_dir)
 
-        for current_dir, subdirs, files in os.walk(user_root_dir):
+        for current_dir, subdirs, files in os.walk(user_files_dir):
             try:
                 parent_dir = Directory.objects.get(name=os.path.basename(
                     current_dir), owner=user, path=current_dir)
