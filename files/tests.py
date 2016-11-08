2ad42a248fe0a7d19b6a750f49c8f62d3816b899
shunyi <M157q.tw@gmail.com>
2016-11-08 21:26:10 +0800

    [#1] Add testings for management command 'scan' (scanner)
    + test files and dirs should be scanned
    + test files and dirs should not be scanned
    + test owner of files and dirs
    + test updated_time
    + test created_time
    + test parent of node

--- files/tests.py
+++ files/tests.py
@@ -5,11 +5,22 @@ from django.core.management import call_command
 from django.db import models
 from django.test import TestCase
 
-from files.models import FYNode
+from files.models import File, Directory
 
 
 class ManagementCommandsScanTest(TestCase):
     def setUp(self):
+        '''
+        user_home_dir/
+            dx/
+            fx
+            files/
+                a
+                b/
+                    c
+                    d/
+        '''
+
         self.user = User.objects.create_user(username='fake-user-for-testing', email='test@test.com', password='test')
         user_home_dir = self.user.profile.home_dir_path
 
@@ -36,19 +47,66 @@ class ManagementCommandsScanTest(TestCase):
 
         call_command('scan', self.user.username)
 
-    def test_file_and_dir_should_not_be_scanned(self):
+    def test_file_should_not_be_scanned(self):
         with self.assertRaises(models.ObjectDoesNotExist):
-            FYNode.objects.get(name='fx')
+            File.objects.get(name='fx')
 
+    def test_dir_should_not_be_scanned(self):
         with self.assertRaises(models.ObjectDoesNotExist):
-            FYNode.objects.get(name='dx')
+            Directory.objects.get(name='dx')
 
-    def test_file_and_dir_should_be_scanned(self):
-        self.assertIsInstance(FYNode.objects.get(name='files'), FYNode)
-        self.assertIsInstance(FYNode.objects.get(name='a'), FYNode)
-        self.assertIsInstance(FYNode.objects.get(name='b'), FYNode)
-        self.assertIsInstance(FYNode.objects.get(name='c'), FYNode)
-        self.assertIsInstance(FYNode.objects.get(name='d'), FYNode)
+    def test_files_should_be_scanned(self):
+        for file_name in ('a', 'c'):
+            self.assertIsInstance(File.objects.get(name=file_name), File)
+
+    def test_dirs_should_be_scanned(self):
+        for dir_name in ('files', 'b', 'd'):
+            self.assertIsInstance(Directory.objects.get(name=dir_name), Directory)
+
+    def test_owner_of_files(self):
+        for file_name in ('a', 'c'):
+            node = File.objects.get(name=file_name)
+            self.assertEqual(self.user.id, node.owner_id)
+
+    def test_owner_of_dirs(self):
+        for dir_name in ('files', 'b', 'd'):
+            node = Directory.objects.get(name=dir_name)
+            self.assertEqual(self.user.id, node.owner_id)
+
+    def test_created_time(self):
+        for f in File.objects.all():
+            self.assertTrue(f.created)
+
+        for d in Directory.objects.all():
+            self.assertTrue(d.created)
+
+    def test_updated_time(self):
+        for f in File.objects.all():
+            self.assertTrue(f.updated)
+
+        for d in Directory.objects.all():
+            self.assertTrue(d.updated)
+
+    def test_parent(self):
+        # The parent of file c and dir d is dir b
+        self.assertEqual(
+            Directory.objects.get(name='b').id,
+            File.objects.get(name='c').parent.id,
+        )
+        self.assertEqual(
+            Directory.objects.get(name='b').id,
+            Directory.objects.get(name='d').parent.id,
+        )
+
+        # The parent of file a and dir b is dir files
+        self.assertEqual(
+            Directory.objects.get(name='files').id,
+            File.objects.get(name='a').parent.id,
+        )
+        self.assertEqual(
+            Directory.objects.get(name='files').id,
+            Directory.objects.get(name='b').parent.id,
+        )
 
     def tearDown(self):
         self.user.delete()
