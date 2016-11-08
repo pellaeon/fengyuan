b001ad1b63729b66dd88ca52d43171c06bfefc90
shunyi <M157q.tw@gmail.com>
2016-11-08 21:21:19 +0800

    [#1] Make scanner record the parent of Directory.

--- files/management/commands/scan.py
+++ files/management/commands/scan.py
@@ -9,12 +9,19 @@ from files.models import Directory, File
 
 # https://docs.djangoproject.com/en/1.10/howto/custom-management-commands/
 class Command(BaseCommand):
-    help = 'Recursively scans the directories and files of the "files/" dir for a specific username, and save those direcoties and files into database. '
+    help = (
+        'Recursively scans the directories and files '
+        'of the "files/" dir for a specific username, '
+        'and save those direcoties and files into database.'
+    )
     USER_FILES_DIR_NAME = 'files'
 
     def add_arguments(self, parser):
-        parser.add_argument('username', type=str,
-                            help='The username which exists in the User model.')
+        parser.add_argument(
+            'username',
+            type=str,
+            help='The username which exists in the User model.',
+        )
 
     def handle(self, *args, **options):
         username = options['username']
@@ -23,7 +30,10 @@ class Command(BaseCommand):
             user = User.objects.get(username=username)
         except:
             raise CommandError(
-                "username: {} doesn't exists in the User model.".format(username))
+                (
+                    "username: {} doesn't exists in the User model."
+                ).format(username)
+            )
 
         user_dir = user.profile.home_dir_path
         if not os.path.exists(user_dir):
@@ -33,24 +43,43 @@ class Command(BaseCommand):
         if not os.path.exists(user_files_dir):
             os.makedirs(user_files_dir)
 
-        for current_dir, subdirs, files in os.walk(user_files_dir):
+        parent_dir = None
+        for current_dir_path, subdirs, files in os.walk(user_files_dir):
             try:
-                parent_dir = Directory.objects.get(name=os.path.basename(
-                    current_dir), owner=user, path=current_dir)
+                current_dir = Directory.objects.get(
+                    name=os.path.basename(current_dir_path),
+                    owner=user,
+                    path=current_dir_path,
+                )
             except ObjectDoesNotExist:
-                parent_dir = Directory(name=os.path.basename(
-                    current_dir), owner=user, path=current_dir)
-                parent_dir.save()
+                current_dir = Directory(
+                    parent=parent_dir,
+                    name=os.path.basename(current_dir_path),
+                    owner=user,
+                    path=current_dir_path,
+                )
+                current_dir.save()
             except:
                 raise
 
-            for file in files:
+            for _file in files:
                 try:
                     f = File.objects.get(
-                        parent=parent_dir, name=file, owner=user)
+                        parent=current_dir,
+                        name=_file,
+                        owner=user,
+                    )
                 except ObjectDoesNotExist:
-                    f = File(parent=parent_dir, name=file, owner=user,
-                             size=os.path.getsize(os.path.join(current_dir, file)))
+                    f = File(
+                        parent=current_dir,
+                        name=_file,
+                        owner=user,
+                        size=os.path.getsize(
+                            os.path.join(current_dir_path, _file)
+                        ),
+                    )
                     f.save()
                 except:
                     raise
+
+            parent_dir = current_dir
